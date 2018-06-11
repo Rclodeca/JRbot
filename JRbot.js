d6a17d7dfdc4175b6e0079cf1b6fae84f56332cb
Rclodeca <ryan@theclodes.com>
2018-06-11 01:45:37 -0700

    updated mute command

--- JRbot.js
+++ JRbot.js
@@ -109,6 +109,9 @@ client.on("message", async message => {
 			let toMute = message.guild.member(message.mentions.users.first());
 			if (!toMute) return message.channel.send("No specified user.");
 
+			if (toMute.highestRole.position >= message.member.highestRole.position) 
+				return message.channel.send("Can't mute a member of a higher or equal role.");
+
 			let role = message.guild.roles.find(r => r.name === "Muted");
 			if(!role) {
 				try {
@@ -145,6 +148,9 @@ client.on("message", async message => {
 
 			let toMute = message.guild.member(message.mentions.users.first());
 			if (!toMute) return message.channel.send("No specified user.");
+			if (toMute.highestRole.position > message.member.highestRole.position) 
+				return message.channel.send("Can't unmute a member of a higher role.");
+			
 
 			let role = message.guild.roles.find(r => r.name === "Muted");
 		
