98bdd2f841167f7fc71a74c6f0bb205d38d53be7
Rclodeca <ryan@theclodes.com>
2018-06-11 23:38:55 -0700

    cleaned up autoreact

--- cmds/autoreact.js
+++ cmds/autoreact.js
@@ -1,12 +1,9 @@
 module.exports.run = async (client, message, args) => {
 	console.log("autoReact is working");
-	const { prefix, token, insultsArray, kaije, cameron } = require('./../config.json');
+	const { prefix, token, insultsArray, kaije } = require('./../config.json');
 
-	message.channel.send("hi");
+	if(message.author.bot) return;
 
-	if(message.author.bot) {
-		return;
-	}
 	var send;
 	switch(message.content) {
 		case "hello":
@@ -18,23 +15,8 @@ module.exports.run = async (client, message, args) => {
 		case kaije:
 			send = kaije;
 			break;	
-		case cameron:
-			send = cameron;
-			break;
 		default:
-			counter++;
-			if (counter === 100) {
-				send = `"${message.content}" :joy: :ok_hand:`;
-				counter = 0;
-			} 
-			else if (counter % 30 === 0) {
-				message.react(message.guild.emojis.get('442122186871013396'))
-				return;
-			}
-			else {
-				return;
-			}
-
+			return;			
 	}
 	message.channel.send(send);
 }
