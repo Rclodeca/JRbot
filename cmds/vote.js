8729f85948ecba37e9dfea4722dac91720113807
Rclodeca <ryan@theclodes.com>
2018-06-11 22:49:22 -0700

    add vote.js

---
+++ cmds/vote.js
@@ -0,0 +1,16 @@
+module.exports.run = async (client, message, args) => {
+/*	const voteMessage = args.join(" ");
+
+	message.channel.send(`VOTE: ${voteMessage}`);
+	message.react('👍');
+	message.react('👎');
+
+	const filter = (reaction) => reaction.emoji.name === '👍';
+	message.awaitReactions(filter, { time: 15000 })
+	.then(collected => console.log(`Collected ${collected.size} reactions`))
+	.catch(console.error);*/
+}
+
+module.exports.help = {
+	name: "vote"
+}
